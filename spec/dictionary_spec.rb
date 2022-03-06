9a802f49bf3a2ad3f275942569e40419414313a6
Cory Bethune <corybbethune@gmail.com>
2022-03-06 14:15:32 -0700

    Create braille to english hash

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -13,14 +13,18 @@ RSpec.describe Dictionary do
 
   it 'has attributes' do
     expect(@dictionary.dict.class).to eq(Hash)
-    expect(@dictionary.dict.length).to eq 39
+    expect(@dictionary.dict.length).to eq 35
     expect(@dictionary.dict.keys.first).to eq "a"
+    expect(@dictionary.braille.values.first).to eq "a"
+    expect(@dictionary.braille.length).to eq 35
+    expect(@dictionary.braille.class).to eq(Hash)
   end
 
-  it 'can be inverted' do
-    expect(@dictionary.braille.class).to eq Hash
-    expect(@dictionary.braille.length).to eq 39
-    expect(@dictionary.braille.values.first).to eq "a"
-  end
+  # it 'can be inverted' do
+  #   expect(@dictionary.braille.class).to eq Hash
+  #   binding.pry
+  #   expect(@dictionary.braille.length).to eq 39
+  #   expect(@dictionary.braille.values.first).to eq "a"
+  # end
 
 end
